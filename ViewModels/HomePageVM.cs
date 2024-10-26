4678d3fa2e81eaa34a0470c3a709df40145b3146
Parniuk Maryia <107621519+cinnaamonka@users.noreply.github.com>
2024-10-26 20:52:51 +0200

    add: ability to switch between two pages

--- ViewModels/HomePageVM.cs
+++ ViewModels/HomePageVM.cs
@@ -2,6 +2,7 @@
 using System.Windows.Controls;
 using AssetManager.Views;
 using AssetManager.Models;
+using CommunityToolkit.Mvvm.Input;
 
 namespace AssetManager.ViewModels
 {
@@ -9,14 +10,22 @@ namespace AssetManager.ViewModels
     {
         public MainPageVM MainPageVM { get; }
 
+        public RelayCommand OpenOverviewPageCommand { get; set; }
+
         public HomePageVM()
         {
-            int a = 0;
+            
         }
 
         public HomePageVM(MainPageVM mainPageVM)
         {
             MainPageVM = mainPageVM;
+            OpenOverviewPageCommand = new RelayCommand(OpenOverviewPage); 
+        }
+
+        private void OpenOverviewPage()
+        {
+            MainPageVM.OpenOverViewPage();
         }
     }
 }
